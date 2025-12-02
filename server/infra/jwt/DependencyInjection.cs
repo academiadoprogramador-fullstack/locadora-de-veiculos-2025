ec22250a58ee100c089a1aedc18b51e587839007
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-12-02 11:41:21 -0300

    server: Adicionado commit inicial

---
+++ server/infra/jwt/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using LocadoraDeVeiculos.Infraestrutura.Jwt.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Jwt;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCamadaInfraestruturaJwt(this IServiceCollection services)
+    {
+        services.AddScoped<AccessTokenProvider>();
+        services.AddScoped<RefreshTokenProvider>();
+
+        return services;
+    }
+}
---
+++ server/infra/orm/Compartilhado/AppDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.Compartilhado;
+
+public static class AppDbContextFactory
+{
+    public static AppDbContext CriarDbContext(string connectionString)
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlServer(connectionString, opt => opt.EnableRetryOnFailure(3))
+            .Options;
+
+        var dbContext = new AppDbContext(options);
+
+        return dbContext;
+    }
+}

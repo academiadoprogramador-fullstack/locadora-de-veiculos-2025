f3b2fba9767d7bef2e66c6c2e82000ab6dd4c495
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-12-03 11:40:18 -0300

    (client): Adicionado projeto base, instalados Angular Material, Tailwind, etc.

---
+++ client/src/app/app.config.ts
@@ -0,0 +1,18 @@
+import {
+    ApplicationConfig, DEFAULT_CURRENCY_CODE, LOCALE_ID, provideBrowserGlobalErrorListeners,
+    provideZonelessChangeDetection
+} from '@angular/core';
+import { provideRouter, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+  ],
+};
---
+++ client/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  template: `
+    <main class="container-fluid py-3">
+      <h1 class="text-4xl">Hello, World!</h1>
+    </main>
+  `,
+})
+export class App {}
